54404153315e94aff68cf27f6f2cea541dcbb03f
Artem Eroshenko <eroshenkoam@qameta.io>
2017-02-19 10:59:21 +0300

    Sonar fixes

--- src/main/java/ru/yandex/qatools/allure/jenkins/tools/AllureCommandlineInstallation.java
+++ src/main/java/ru/yandex/qatools/allure/jenkins/tools/AllureCommandlineInstallation.java
@@ -25,7 +25,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Artem Eroshenko <eroshenkoam@yandex-team.ru>
