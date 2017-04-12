87b9bd44731ac3a95db7a004691a92a44f67ec87
Artem Eroshenko <eroshenkoam@qameta.io>
2017-04-12 16:33:56 +0300

    Fix version problem

--- src/main/java/ru/yandex/qatools/allure/jenkins/ReportBuilder.java
+++ src/main/java/ru/yandex/qatools/allure/jenkins/ReportBuilder.java
@@ -8,9 +8,7 @@ import hudson.util.ArgumentListBuilder;
 import ru.yandex.qatools.allure.jenkins.tools.AllureCommandlineInstallation;
 
 import javax.annotation.Nonnull;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.List;
 
 /**
@@ -39,7 +37,7 @@ public class ReportBuilder {
 
     public int build(@Nonnull List<FilePath> resultsPaths, @Nonnull FilePath reportPath) //NOSONAR
             throws IOException, InterruptedException {
-        String version = getVersion();
+        String version = commandline.getMajorVersion(launcher);
         ArgumentListBuilder arguments = getArguments(version, resultsPaths, reportPath);
 
         return launcher.launch().cmds(arguments)
@@ -48,8 +46,8 @@ public class ReportBuilder {
 
     private ArgumentListBuilder getArguments(String version, @Nonnull List<FilePath> resultsPaths, @Nonnull FilePath reportPath)
             throws IOException, InterruptedException {
-        return version.startsWith("1") ? getAllure1Arguments(resultsPaths, reportPath) :
-                getAllure2Arguments(resultsPaths, reportPath);
+        return version.startsWith("2") ? getAllure2Arguments(resultsPaths, reportPath) :
+                getAllure1Arguments(resultsPaths, reportPath);
     }
 
     private ArgumentListBuilder getAllure2Arguments(@Nonnull List<FilePath> resultsPaths, @Nonnull FilePath reportPath) //NOSONAR
@@ -76,18 +74,6 @@ public class ReportBuilder {
         arguments.add("-o");
         arguments.addQuoted(reportPath.getRemote());
         return arguments;
-    }
-
-    private String getVersion() throws IOException, InterruptedException {
-        ArgumentListBuilder arguments = new ArgumentListBuilder();
-        arguments.add(commandline.getExecutable(launcher));
-        arguments.add("version");
-        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
-            launcher.launch().cmds(arguments).stdout(stream).pwd(workspace).join();
-            String version = new String(stream.toByteArray(), Charset.forName("UTF-8"));
-            listener.getLogger().format("Allure version: %s", version);
-            return version;
-        }
     }
 
 }
