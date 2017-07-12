c6f7748d440c172c2726b355ff254658773145c1
Artem Eroshenko <eroshenkoam@qameta.io>
2017-07-12 16:45:13 +0300

    fix compilation

--- src/main/java/ru/yandex/qatools/allure/jenkins/AllureReportPublisher.java
+++ src/main/java/ru/yandex/qatools/allure/jenkins/AllureReportPublisher.java
@@ -41,6 +41,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.zip.ZipEntry;
