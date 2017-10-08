0cf6f62d4f495c8ee42b4648c202a744f61494ca
bvo2002 <bvo2002@gmail.com>
2017-10-08 17:08:41 +0300

    correction

--- src/test/java/ru/yandex/qatools/allure/jenkins/ReportGenerateIT.java
+++ src/test/java/ru/yandex/qatools/allure/jenkins/ReportGenerateIT.java
@@ -189,7 +189,7 @@ public class ReportGenerateIT {
 
     private AllureReportPublisher createAllurePublisherWithoutCommandline(String... resultsPaths) throws Exception {
         final List<ResultsConfig> results = ResultsConfig.convertPaths(Arrays.asList(resultsPaths));
-        final AllureReportPublisher publisher = new AllureReportPublisher(results);
+        final AllureReportPublisher publisher = new AllureReportPublisher(results, true);
         publisher.setJdk(jdk);
         return publisher;
     }
