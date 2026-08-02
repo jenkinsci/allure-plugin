a4f33081e28c97025bc47d3e57a35f0709c34d83
Tim Jacomb <timjacomb1@gmail.com>
2026-08-02 23:36:11 +0100

    Migrate from Commons Lang 2 to Commons Lang 3

--- src/main/java/org/allurereport/jenkins/config/AllureReportConfig.java
+++ src/main/java/org/allurereport/jenkins/config/AllureReportConfig.java
@@ -16,7 +16,7 @@
 package org.allurereport.jenkins.config;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
